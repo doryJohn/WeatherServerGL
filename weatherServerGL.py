568ed5e88fa06cc3fa5ed7224849c888c87f65c3
doryJohn <14955527+doryJohn@users.noreply.github.com>
2024-08-04 12:30:49 -0400

    Update weatherServerGL.py

--- weatherServerGL.py
+++ weatherServerGL.py
@@ -3,20 +3,48 @@ import json
 import FTPLoadFile as FTP
 import FTPparse as MyParse
 import sendEmail as email
+import GetCanadianWeather as GetCan
 
 def lambda_handler(event, context):
+    ToAddresses = ["johnwilliamturley@yahoo.com"]  #this is for diagnostic purposes
+
+#### Start with NOAA summary and lsz266
     forecastID="lsz266"
-    ToAddresses = ["johnwilliamturley@yahoo.com", "iterluxlucis@gmail.com", "JohnOnSiren@zoleo.com"]
-    # TODO implement
     myBuff = FTP.FTPLoad(forecastID)
-    print("full text")
-    print(myBuff)
     summaryTxt, forcastTxt =  MyParse.ParseFTPbuff(myBuff,forecastID)
-    print("Summary:")
-    print(summaryTxt)
-    print("Forcast:")
-    print(forcastTxt)
-    email.sendEmail(ToAddresses, summaryTxt)
+#  send out summaryTxt
+    email.sendEmail(ToAddresses, summaryTxt)    #send to monitering emails
+#    split into chunks and send to zoleo
+    chunk_size = 223
+    for i in range(0, len(summaryTxt), chunk_size):
+        email.sendEmail(["JohnOnSiren@zoleo.com"],summaryTxt[i:i+chunk_size]) 
+#### Now do Canadian Forcast
+    canforcastTxt = GetCan.GetForcast()
+    email.sendEmail(ToAddresses, canforcastTxt)    #send to monitering emails
+ #    split into chunks and send to zoleo
+    chunk_size = 223
+    for i in range(0, len(canforcastTxt), chunk_size):
+        email.sendEmail(["JohnOnSiren@zoleo.com"],canforcastTxt[i:i+chunk_size])
+#### now go back to forcastTxt for lsz266 and send it out
+    email.sendEmail(ToAddresses, forcastTxt)    #send to monitering emails
+    
+ #    split into chunks and send to zoleo
+    chunk_size = 223
+    for i in range(0, len(forcastTxt), chunk_size):
+        email.sendEmail(["JohnOnSiren@zoleo.com"],forcastTxt[i:i+chunk_size])
+
+       
+#### Now do NOAA lsz267      
+    forecastID="lsz267"
+    myBuff = FTP.FTPLoad(forecastID)
+    summaryTxt, forcastTxt =  MyParse.ParseFTPbuff(myBuff,forecastID)
+  # Send out forcastTxt first
+    email.sendEmail(ToAddresses, forcastTxt)    #send to monitering emails
+ #    split into chunks and send to zoleo
+    chunk_size = 223
+    for i in range(0, len(forcastTxt), chunk_size):
+        email.sendEmail(["JohnOnSiren@zoleo.com"],forcastTxt[i:i+chunk_size])
+
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
